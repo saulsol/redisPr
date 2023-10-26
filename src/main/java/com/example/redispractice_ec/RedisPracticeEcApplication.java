d8c5db728d5ccd4f91ff8f1b81bbf23cc0cbdccb
saul <dlathf3210@naver.com>
2023-10-26 21:10:43 +0900

    [START]

---
+++ src/main/java/com/example/redispractice_ec/RedisPracticeEcApplication.java
@@ -0,0 +1,13 @@
+package com.example.redispractice_ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPracticeEcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisPracticeEcApplication.class, args);
+    }
+
+}
