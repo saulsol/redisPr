104a799d20d4a7896ec26cabd72cb392fb3b68dd
saul <dlathf3210@naver.com>
2023-10-30 00:13:48 +0900

    GetMapping getZSETValue

--- src/main/java/com/example/redispractice_ec/controller/LowestPriceController.java
+++ src/main/java/com/example/redispractice_ec/controller/LowestPriceController.java
@@ -1,12 +1,23 @@
 package com.example.redispractice_ec.controller;
 
+import com.example.redispractice_ec.service.LowestPriceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("")
 public class LowestPriceController {
 
+    @Autowired
+    private LowestPriceService lowestPriceService;
 
-
+    //http://localhost:8080/getZSETValue?key=highScores
+    @GetMapping("/getZSETValue")
+    public Set<?> getZSETValue(String key){
+        return lowestPriceService.getZSETValue(key);
+    }
 }
--- src/main/java/com/example/redispractice_ec/service/LowestPriceService.java
+++ src/main/java/com/example/redispractice_ec/service/LowestPriceService.java
@@ -1,9 +1,23 @@
 package com.example.redispractice_ec.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
+@RequiredArgsConstructor
 public class LowestPriceService {
 
+    private final RedisTemplate<String, Object> redisTemplate;
 
+    public Set<?> getZSETValue(String key){
+        Set<?> myTempSet = new HashSet<>();
+        myTempSet = redisTemplate.opsForZSet().rangeWithScores(key, 0, 9);
+        return myTempSet;
+    }
 }
